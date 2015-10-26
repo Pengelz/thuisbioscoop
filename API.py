8b19c78dbd96c3d06df742b37103c632164c37c7
MuxBee <mbeunk@live.nl>
2015-10-26 14:09:06 +0100

    Correctie
    Datetime aangepast en print weggehaald

--- API.py
+++ API.py
@@ -4,10 +4,9 @@ import xmltodict
 
 def ophalenFilms(invoer):
     if invoer == "Vandaag":
-        datum = datetime.strftime("%d-%m-%Y")
+        datum = (datetime.date.today()).strftime("%d-%m-%Y")
     else:
         datum = (datetime.date.today() + datetime.timedelta(days=1)).strftime("%d-%m-%Y")
-        print(datum)
     url = "http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=rilqhtllly1ugzm552twhnzv7c6x50xl&dag="+ datum +"&sorteer=0"
 
     request = requests.get(url)
