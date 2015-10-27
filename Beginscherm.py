bcfe5f08ea5e7a7e490115aba6ef1491ca1a9910
MuxBee <mbeunk@live.nl>
2015-10-27 11:28:56 +0100

    Aanpassingen bestanden voor login
    Van beginscherm oproepbare frame gemaakt zodat vanuit login het
    beginscherm opgeroepen kan worden. Aanpassingen in login frame zodat het
    beginscherm oproept ipv gegevens tonen

--- Beginscherm.py
+++ Beginscherm.py
@@ -2,39 +2,41 @@ from tkinter import *
 import API
 from tkinter.messagebox import showinfo
 
-window = Tk()
+def beginscherm():
 
-textLabel=Label(master=window, background="Green", width=50, height=40)
-textLabel.pack(side=LEFT)
+    window = Tk()
 
-def klik_vandaag():
-    dictionary = API.ophalenFilms("Vandaag")
+    textLabel=Label(master=window, background="Green", width=50, height=40)
+    textLabel.pack(side=LEFT)
 
-    films = dictionary['filmsoptv']['film']
-    y=50
-    for film in films:
-        vandaagLabel = Label(master=window, text=film['titel'])
-        vandaagLabel.place(x=30, y=y)
-        y+=20
+    def klik_vandaag():
+        dictionary = API.ophalenFilms("Vandaag")
 
-def klik_morgen():
-    dictionary = API.ophalenFilms("Morgen")
+        films = dictionary['filmsoptv']['film']
+        y=50
+        for film in films:
+            vandaagLabel = Label(master=window, text=film['titel'])
+            vandaagLabel.place(x=30, y=y)
+            y+=20
 
-    films = dictionary['filmsoptv']['film']
-    y=50
-    for film in films:
-        vandaagLabel = Label(master=window, text=film['titel'])
-        vandaagLabel.place(x=210, y=y)
-        y+=20
+    def klik_morgen():
+        dictionary = API.ophalenFilms("Morgen")
 
-knop_vandaag = Button(window, text="Vandaag", command=klik_vandaag)
-knop_vandaag.place(x=10, y=10)
+        films = dictionary['filmsoptv']['film']
+        y=50
+        for film in films:
+            vandaagLabel = Label(master=window, text=film['titel'])
+            vandaagLabel.place(x=210, y=y)
+            y+=20
 
-knop_morgen = Button(window, text="Morgen", command=klik_morgen)
-knop_morgen.place(x=200, y=10)
+    knop_vandaag = Button(window, text="Vandaag", command=klik_vandaag)
+    knop_vandaag.place(x=10, y=10)
+
+    knop_morgen = Button(window, text="Morgen", command=klik_morgen)
+    knop_morgen.place(x=200, y=10)
 
 
 
 
 
-window.mainloop()
+    window.mainloop()
---
+++ LoginFrame.py
@@ -0,0 +1,58 @@
+from csv import DictReader
+from tkinter import *
+import tkinter.messagebox as tm
+import Beginscherm
+
+
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.username = ""
+
+        self.label_1 = Label(self, text="Gebruiker")
+        self.label_2 = Label(self, text="Wachtwoord")
+
+        self.entry_1 = Entry(self)
+        self.entry_2 = Entry(self, show="*")
+
+        self.loginbtn = Button(self, text="Login", command=self._login_btn_clicked)
+
+        self.showLogin()
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        username = self.entry_1.get()
+        password = self.entry_2.get()
+
+        if self.login(username, password):
+            tm.showinfo("Login info", "Welkom " + str(self.username))
+            root.destroy()
+            Beginscherm.beginscherm()
+
+        else:
+            tm.showerror("Login error", "Incorrecte gebruikersnaam/wachtwoord")
+
+    def login(self, username, password):
+        with open('database/users.csv') as csvfile:
+            reader = DictReader(csvfile)
+            for row in reader:
+                if row["username"] == username and row["password"] == password:
+                    self.username = row["username"]
+                    return True
+
+            return False
+
+    def showLogin(self):
+        self.label_1.grid(row=0, column=0)
+        self.label_2.grid(row=1, column=0)
+        self.entry_1.grid(row=0, column=1)
+        self.entry_2.grid(row=1, column=1)
+        self.loginbtn.grid(row=2, column=1)
+
+root = Tk()
+loginFrame = LoginFrame(root)
+root.resizable(width=FALSE, height=FALSE)
+root.geometry("800x600")
+root.mainloop()
