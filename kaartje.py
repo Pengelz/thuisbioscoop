4bb8ae8901af67e01023090b2dc617c6cbe76a99
MuxBee <mbeunk@live.nl>
2015-10-28 11:15:42 +0100

    aanbieders toegevoegd

--- kaartje.py
+++ kaartje.py
@@ -3,20 +3,21 @@ import csv
 import qrcode
 from PIL import Image, ImageTk
 
-def show(code, id,titel):
+def show(code, id, titel, aanbieder):
     window = Tk()
     label = Label(master=window, text=code).pack()
-    save(code,id, titel)
+    save(code,id, titel, aanbieder)
     qr = qrcode.make(code)
     image = ImageTk.PhotoImage(qr)
     label2 = Label(image=image)
     label2.image=image
     label2.pack()
     window.mainloop()
-def save(code,id,titel):
+
+def save(code,id,titel, aanbieder):
     file = open('database/gekocht.csv', 'a', newline='')
     write = csv.writer(file)
-    write.writerow((id, code, titel))
+    write.writerow((id, code, titel, aanbieder))
     file.close()
     return None
 
