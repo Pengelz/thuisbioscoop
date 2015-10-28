7512744245560d0552051c602374594826148a1c
MuxBee <mbeunk@live.nl>
2015-10-28 10:21:01 +0100

    Qr code
    Qr code werkt nu, alleen word het toegevoegd op het main scherm, nog
    even naar kijken

--- kaartje.py
+++ kaartje.py
@@ -1,10 +1,18 @@
 from tkinter import *
 import csv
+import qrcode
+from PIL import Image, ImageTk
 
 def show(code, id,titel):
     window = Tk()
     label = Label(master=window, text=code).pack()
     save(code,id, titel)
+    qr = qrcode.make(code)
+    image = ImageTk.PhotoImage(qr)
+    label2 = Label(image=image)
+    label2.image=image
+    label2.pack()
+    print(qr)
     window.mainloop()
 def save(code,id,titel):
     file = open('database/gekocht.csv', 'a', newline='')
@@ -12,3 +20,5 @@ def save(code,id,titel):
     write.writerow((id, code, titel))
     file.close()
     return None
+
+
